ae99aa4ed9b5a75e2f707c6135f06d232f11b9cd
Jelena Grimshaw <muchstudio@hotmail.com>
2019-05-06 19:24:22 +0000

    step 4

---
+++ step-4-print-with-callbacks.js
@@ -0,0 +1,31 @@
+var https = require('https');
+
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step4.html'
+};
+
+function getHTML(options, callback){
+
+https.get(requestOptions, function(response){
+  response.setEncoding('utf8')
+
+  var string = ''
+  response.on('data', function(chunk){
+    // console.log('Chunk received. Length: ', chunk.length)
+    string += chunk
+    printHTML(chunk)
+
+    console.log(chunk.length)
+    console.log(chunk + '\n')
+  })
+
+})
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+
+getHTML()
